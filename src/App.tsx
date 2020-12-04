dd3421d9dc46cf17ab59b888d42b1e250f775707
Navin <navin007.a@gmail.com>
2020-12-04 19:34:56 +0530

    feat(linter): ✨  add linters & formatters

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Typescript Project Template</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
