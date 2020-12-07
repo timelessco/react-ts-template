5a8989f7ea5bc079c8c1a11eca6555ebde82eaba
Navin <navin007.a@gmail.com>
2020-12-07 13:15:14 +0530

    refactor(src): ♻️  remove extra boilerplates
    Fix https://github.com/facebook/create-react-app/issues/10144

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Hello World/i);
+  expect(h1Element).toBeInTheDocument();
 });
