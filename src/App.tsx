6dc5c45c215c3cfb0b6236dc10a9c4fff3a76ff4
Navin <navin007.a@gmail.com>
2020-12-07 12:12:29 +0530

    feat(cra): ✨  add cra to the template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import * as React from "react";
-import "./styles.css";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>React Typescript Project Template</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
